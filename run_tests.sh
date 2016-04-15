e66df49419fb1df6e16e323343482675812b9c75
Samuel Gélineau <gelisam+github@gmail.com>
2016-04-15 00:57:10 -0400

    also run tests in subfolders

--- run_tests.sh
+++ run_tests.sh
@@ -3,4 +3,4 @@ set -e
 clear
 DIR="$(dirname "$0")"
 cabal build
-"$DIR"/.cabal-sandbox/bin/doctest -i"$DIR"/src "$DIR"/src/*.hs
+"$DIR"/.cabal-sandbox/bin/doctest -i"$DIR"/src $(find "$DIR/src" -name '*.hs')
