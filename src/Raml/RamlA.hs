17a14af64deb9f7e9363f020efed94b9f1434664
Samuel Gélineau <gelisam+github@gmail.com>
2016-04-15 01:08:39 -0400

    RamlB, an alternative to Syntax.Semantics with a better interface

--- src/Raml/RamlA.hs
+++ src/Raml/RamlA.hs
@@ -1,5 +1,8 @@
 module Raml.RamlA where
 
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Reader
+import Data.Functor.Identity
 import qualified Data.Yaml as Yaml
 import qualified Data.Map.Strict as Map
 import           Data.Map.Strict (Map, (!))
@@ -7,18 +10,30 @@ import           Data.Map.Strict (Map, (!))
 import Raml.Syntax
 
 
-type RamlA a = RamlFile -> a
+type RamlAT = ReaderT RamlFile
+type RamlA = RamlAT Identity
 
-runRamlA :: FilePath -> RamlA a -> IO a
-runRamlA filePath f = do
-    r <- Yaml.decodeFileEither filePath
+runRamlAT :: MonadIO m => FilePath -> RamlAT m a -> m a
+runRamlAT filePath ramlAT = do
+    r <- liftIO $ Yaml.decodeFileEither filePath
     case r of
       Left exception -> fail (show exception)
-      Right ramlFile -> return (f ramlFile)
+      Right ramlFile -> runReaderT ramlAT ramlFile
 
+ramlFileA :: Monad m => RamlAT m RamlFile
+ramlFileA = ask
 
-getNamedTypes :: RamlA [String]
-getNamedTypes = Map.keys . types
+typesA :: Monad m => RamlAT m (Map String TypeDesc)
+typesA = types <$> ramlFileA
 
-lookupType :: String -> RamlA TypeDesc
-lookupType k r = types r ! k
+-- |
+-- >>> runRamlAT "tests/sample.in" namedTypesA
+-- ["Alternative","BooleanType","DataType","DateType","Field","NumberType","StringType"]
+namedTypesA :: Monad m => RamlAT m [String]
+namedTypesA = Map.keys <$> typesA
+
+-- |
+-- >>> runRamlAT "tests/sample.in" (parentType <$> lookupTypeA "DataType")
+-- Union [NamedType "StringType",NamedType "NumberType",NamedType "DateType",NamedType "BooleanType"]
+lookupTypeA :: Monad m => String -> RamlAT m TypeDesc
+lookupTypeA k = (! k) <$> typesA
