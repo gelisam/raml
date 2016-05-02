a1f901485e0fd703d8a987da2b8848a4c50863b9
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-05-02 13:36:27 -0400

    move shared types to Raml.Common

--- src/Raml/Parser.hs
+++ src/Raml/Parser.hs
@@ -1,28 +1,16 @@
 {-# LANGUAGE OverloadedStrings, PatternSynonyms, ViewPatterns #-}
 module Raml.Parser where
 
-import Control.Applicative
 import qualified Data.Aeson as Json
 import qualified Data.List as List
 import           Data.Map (Map)
-import Data.Maybe
-import           Data.Text (Text)
-import qualified Data.Text as Text
 import           Data.Yaml (FromJSON(..), ToJSON(..), (.:), (.:?), (.=))
 import qualified Data.Yaml as Yaml
 import Extra
 import Text.Printf
 
+import Raml.Common
 
-type TypeName = String
-type PropertyName = String
-type Regexp = String
-
--- Either's json encoding is "{Left: ...}",
--- OrElse's json encoding is "..."
-newtype OrElse a b = OrElse
-  { unOrElse :: Either a b
-  } deriving (Show, Eq)
 
 data TypeExpr
   = Object
@@ -42,29 +30,6 @@ newtype ParseTree = ParseTree
   { unParseTree :: Map TypeName (OrElse TypeExpr TypeProps)
   } deriving (Show, Eq)
 
-
-pattern YamlString s <- Yaml.String (Text.unpack -> s) where
-  YamlString s = Yaml.String (Text.pack s)
-
--- Yaml.object ["foo" .= Nothing] is {"foo": null},
--- object ["foo" .=? Nothing] is {}.
-object :: [Maybe (Text, Yaml.Value)] -> Yaml.Value
-object = Yaml.object . catMaybes
-
-(.=?) :: ToJSON a => Text -> Maybe a -> Maybe (Text, Yaml.Value)
-key .=? value = do
-    v <- value
-    return (key, toJSON v)
-
-
-instance (FromJSON a, FromJSON b) => FromJSON (OrElse a b) where
-  parseJSON v = OrElse <$> ( Left  <$> parseJSON v
-                         <|> Right <$> parseJSON v
-                           )
-
-instance (ToJSON a, ToJSON b) => ToJSON (OrElse a b) where
-  toJSON (OrElse (Left  x)) = toJSON x
-  toJSON (OrElse (Right y)) = toJSON y
 
 instance FromJSON TypeExpr where
   parseJSON (YamlString s) = case wordsBy (`elem` [' ', '|']) s of
