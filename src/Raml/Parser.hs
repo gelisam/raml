3178e7d66cb1ec267613ea5eef70b8aa3c1cda38
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-05-02 11:14:43 -0400

    ParseTree

--- src/Raml/Parser.hs
+++ src/Raml/Parser.hs
@@ -1,9 +1,40 @@
 module Raml.Parser where
 
+import Data.Map as Map
 import Data.Yaml as Yaml
 
 
-data ParseTree = ParseTree
+type TypeName = String
+type PropertyName = String
+type Regexp = String
+
+data TypeExpr
+  = Object
+  | String
+  | Ref TypeName
+  | Union [TypeExpr]
+  deriving (Show, Eq)
+
+data SharedProps = SharedProps
+  { properties :: Map PropertyName PropertyProps
+  , discriminator :: Maybe PropertyName
+  , stringPattern :: Maybe Regexp
+  } deriving (Show, Eq)
+
+data PropertyProps = PropertyProps
+  { type_ :: TypeExpr
+  , sharedPropertyProps :: SharedProps
+  } deriving (Show, Eq)
+
+data TypeProps = TypeProps
+  { parentType :: TypeExpr
+  , sharedTypeProps :: SharedProps
+  } deriving (Show, Eq)
+
+newtype ParseTree = ParseTree
+  { unParseTree :: Map TypeName TypeProps
+  } deriving (Show, Eq)
+
 
 parse :: Yaml.Value -> ParseTree
-parse _ = ParseTree
+parse _ = undefined
