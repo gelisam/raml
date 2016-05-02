6901073a800a21199f770833924d8212cf045d43
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-05-02 14:53:32 -0400

    ClassifiedTree

--- src/Raml/Classifier.hs
+++ src/Raml/Classifier.hs
@@ -1,9 +1,53 @@
 module Raml.Classifier where
 
-import Raml.Normalizer
+import           Data.Map (Map)
+
+import Raml.Common
+import           Raml.Normalizer (NormalizedTree(..))
 
 
-data ClassifiedTree = ClassifiedTree
+data ObjectType
+  = Object
+  | ObjectRef TypeName
+  deriving (Show, Eq)
+
+data StringType
+  = String
+  | StringRef TypeName
+  deriving (Show, Eq)
+
+type UnionType = [Either ObjectType StringType]
+
+data Type
+  = ObjectType ObjectType
+  | StringType StringType
+  | UnionType UnionType
+
+
+data ObjectProps = ObjectProps
+  { parentObjectType :: ObjectType
+  , properties :: Map PropertyName TypeProps
+  , objectDiscriminator :: Maybe PropertyName
+  } deriving (Show, Eq)
+
+data StringProps = StringProps
+  { parentStringType :: StringType
+  , stringPattern :: Maybe Regexp
+  } deriving (Show, Eq)
+
+type UnionProps = UnionType
+
+data TypeProps
+  = ObjectTypeProps ObjectProps
+  | StringTypeProps StringProps
+  | UnionTypeProps UnionProps
+  deriving (Show, Eq)
+
+
+newtype ClassifiedTree = ClassifiedTree
+  { unClassifiedTree :: Map TypeName TypeProps
+  } deriving (Show, Eq)
+
 
 classify :: NormalizedTree -> ClassifiedTree
-classify _ = ClassifiedTree
+classify _ = undefined
