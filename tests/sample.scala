62194a928b5248c8442b8e02d91be05be1095495
Samuel Gélineau <gelisam+github@gmail.com>
2016-05-03 12:01:20 -0400

    the scala code I would like to generate
    ignoring the discriminator and the json serialization for now

---
+++ tests/sample.scala
@@ -0,0 +1,24 @@
+sealed trait DataType
+case object StringType extends DataType
+case object NumberType extends DataType
+case class DateType(
+  dateFormat: String
+) extends DataType {
+  require(
+    dateFormat match {
+      case DateType.DateFormatPattern() => true
+      case _ => false
+    }
+  )
+}
+case object BooleanType extends DataType
+
+object DateType {
+  val DateFormatPattern = "[YMD]+[-\\.][YMD]+[-\\.\\/][YMD]+".r
+}
+
+
+case class Field(
+  name: String,
+  dataType: DataType
+)
