da724c6e558cdf766e391f1575cab539e282660b
Samuel Gélineau <gelisam+github@gmail.com>
2016-05-08 22:42:10 -0400

    configure the sandbox the first time we build

--- run_tests.sh
+++ run_tests.sh
@@ -2,5 +2,12 @@
 set -e
 clear
 DIR="$(dirname "$0")"
+
+if [ ! -d "$DIR/.cabal-sandbox" ]; then
+  cabal sandbox init
+  cabal install doctest
+  cabal install
+fi
+
 cabal build
 "$DIR"/.cabal-sandbox/bin/doctest -i"$DIR"/src $(find "$DIR/src" -name '*.hs')
