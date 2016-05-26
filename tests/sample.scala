cb314791e8cfe552285e1018e7a77145f28a78ae
Samuel Gélineau <samuel.gelineau@keatext.com>
2016-05-26 16:51:31 -0400

    new goal: also generate jsonFormat implementations

--- tests/sample.scala
+++ tests/sample.scala
@@ -1,5 +1,8 @@
 package com.keatext.gelineau.samuel.hello
 
+import spray.json.DefaultJsonProtocol._
+import spray.json._
+
 
 sealed trait DataType
 case object StringType extends DataType
@@ -16,6 +19,45 @@ case class DateType(
 }
 case object BooleanType extends DataType
 
+object DataType {
+  implicit val jsonFormat: RootJsonFormat[DataType] = new RootJsonFormat[DataType] {
+    override def read(json: JsValue): DataType =
+      json match {
+        case JsObject(jsObject) =>
+          jsObject("discriminator") match {
+            case JsString("StringType") =>
+              StringType
+            case JsString("NumberType") =>
+              NumberType
+            case JsString("DateType") =>
+              val dateFormat = implicitly[JsonFormat[String]].read(jsObject("dateFormat"))
+              DateType(dateFormat)
+            case JsString("BooleanType") =>
+              BooleanType
+            case discriminator =>
+              deserializationError(s"unexpected discriminator ${discriminator}, expected one of ${JsString("StringType")}, ${JsString("NumberType")}, ${JsString("DateType")} or ${JsString("BooleanType")}")
+          }
+        case _ =>
+          deserializationError(s"expected object, got ${json}")
+      }
+
+    override def write(obj: DataType): JsValue =
+      obj match {
+        case StringType =>
+          JsObject("discriminator" -> JsString("StringType"))
+        case NumberType =>
+          JsObject("discriminator" -> JsString("NumberType"))
+        case DateType(dateFormat) =>
+          JsObject(
+            "discriminator" -> JsString("DateType"),
+            "dateFormat" -> JsString(dateFormat)
+          )
+        case BooleanType =>
+          JsObject("discriminator" -> JsString("BooleanType"))
+      }
+  }
+}
+
 object DateType {
   val DateFormatPattern = "[YMD]+[-\\.][YMD]+[-\\.\\/][YMD]+".r
 }
@@ -25,3 +67,7 @@ case class Field(
   name: String,
   dataType: DataType
 )
+
+object Field {
+  implicit val jsonFormat: RootJsonFormat[Field] = jsonFormat2(Field.apply)
+}
